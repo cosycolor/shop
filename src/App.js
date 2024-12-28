01ae27ea4c7ad699484d844d07d61ea7bf0fc922
cosycolor <handyoon87@gmail.com>
2024-12-29 00:03:53 +0900

    routes, nested routes, navigate, useParams

--- src/App.js
+++ src/App.js
@@ -3,31 +3,93 @@ import './App.css';
 import { Button, Container, Navbar, Nav } from 'react-bootstrap';
 import data from './data.js'
 import { useState } from 'react';
+import {Routes, Route, Link, useNavigate, Outlet, useParams} from 'react-router-dom';
+import styled from 'styled-components';
 
 function App() {
   let [shoes] = useState(data);
+  let navigate = useNavigate();
+
+
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link onClick = {() => {navigate('/')}}>Home</Nav.Link>
+            <Nav.Link onClick = {() => {navigate('/detail')}}>Detail</Nav.Link>
+            <Nav.Link onClick = {() => navigate('/about')}>About</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className = "main-bg"></div>
+      <Routes>
+        <Route path="/" element = {<MainPage shoes={shoes}/>}/>
+        <Route path="/detail/:id" element = {<DetailInfo shoes = {shoes} />}/>
+        <Route path="/about" element = {<About />}>
+          <Route path="member" element = {<div><span style={{backgroundColor : "red"}}>멤버들</span></div>}/>
+          <Route path="location" element = {<div><span style={{backgroundColor : "red"}}>회사위치</span></div>}/>
+        </Route>
+        <Route path="*" element = {<div>404 PAGE</div>}/>
+        <Route path="/event" element = { <Event />}>
+          <Route path="one" element = {<p>첫 주문시 양배추즙 서비스</p>}/>
+          <Route path="two" element = {<p>생일기념 쿠폰받기</p>}/>
+        </Route>
+      </Routes>
+    </div>
+    
+  );
+}
+
+function Event(){
+  return (
+    <div>오늘의 이벤트
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function About(){
+  return (
+    <div>
+      <h4>about 페이지</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function MainPage(props){
+  return (
+    <>
+    <div className = "main-bg"></div>
       <div className="container">
         <div className="row">
-          {shoes.map(function(data,i){
+          {props.shoes.map(function(data,i){
             return (<ShoesInfo data = {data} number={i+1}/>);
           })}
         </div>
       </div>
+    </>
+  )}
+
+function DetailInfo(props){
+  let {id} = useParams();
+  let product = props.shoes.find((x)=> x.id == id);
+  return(
+  <div className="container">
+    <div className="row">
+    <div className="col-md-6">
+      <img src={"https://codingapple1.github.io/shop/shoes"+(Number(product.id)+1)+".jpg"} width="100%" />
     </div>
-  );
+    <div className="col-md-6">
+      <h4 className="pt-5">{product.title}</h4>
+      <p>{props.shoes[id].content}</p>
+      <p>{props.shoes[id].price}</p>
+      <button className="btn btn-danger">주문하기</button> 
+    </div>
+    </div>
+  </div> 
+  )
 }
 
 function ShoesInfo(props){
