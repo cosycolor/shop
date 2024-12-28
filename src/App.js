3e4620260b29cbe2dc625e9dbeeacf3ca948992b
cosycolor <handyoon87@gmail.com>
2024-12-28 19:30:39 +0900

    navBar, bootstrap, component, loop, export, import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+/* eslint-disable */
 import './App.css';
 import { Button, Container, Navbar, Nav } from 'react-bootstrap';
+import data from './data.js'
+import { useState } from 'react';
 
 function App() {
+  let [shoes] = useState(data);
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
@@ -14,8 +18,26 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+      <div className = "main-bg"></div>
+      <div className="container">
+        <div className="row">
+          {shoes.map(function(data,i){
+            return (<ShoesInfo data = {data} number={i+1}/>);
+          })}
+        </div>
+      </div>
     </div>
   );
+}
+
+function ShoesInfo(props){
+  return (
+    <div className="col-md-4">
+            <img src={"https://codingapple1.github.io/shop/shoes"+props.number+".jpg"} width="80%"></img>
+            <h4>{props.data.title}</h4>
+            <p>{props.data.price}</p>
+          </div>
+  )
 }
 
 export default App;
